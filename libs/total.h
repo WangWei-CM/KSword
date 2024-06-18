7091d58732ed40e5ea3156521a22afd829f61d2a
Estrella-Explore <Estrella_Explore@outlook.com>
2024-06-18 17:57:27 +0800

    分散至各个模块 (24/6/18)

--- libs/total.h
+++ libs/total.h
@@ -13,3 +13,22 @@
 #include <cctype>
 
 #endif // !_TOTAL_H_
+
+int pw();
+int options();
+int show_logo();
+int shell();
+int loadcmd();
+int wintop(); // no input and output,put window to top
+int winuntop();
+int fread();      // fast read,int i;i=fread();
+void strtochar(); // copy: charusrname,usrname;chardoname,doname
+void cprint(const char *,
+            int); // colorful print not c++ print,only print char,you can change it but i suggest dont do that;
+void ready();     // some loading process;
+void getpcname(std::string &, std::string &);
+void toLowerCase(std::string &, std::string &);
+void cutpth(string, const char);           // has something with cmd"cd" and "cd..".It is already shit so dont touch it.
+bool directoryExists(const std::string &); // input a output a bool.
+
+string getCmdResult(const string &); // get cmd result.Need a command in string'
