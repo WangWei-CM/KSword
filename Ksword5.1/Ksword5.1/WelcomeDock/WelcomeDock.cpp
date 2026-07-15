6fada93380e5dd6f4d62e4c799bb33e70e36a11d
WangWei-CM <3325144899@qq.com>
2026-07-15 11:04:51 +0800

    跳过backup目录、忽略版本号

--- Ksword5.1/Ksword5.1/WelcomeDock/WelcomeDock.cpp
+++ Ksword5.1/Ksword5.1/WelcomeDock/WelcomeDock.cpp
@@ -10,7 +10,7 @@ namespace
     // kReleaseVersionText 作用：
     // - 欢迎页显示的版本号文本；
     // - 由发布脚本按注释标记替换。
-    const QString kReleaseVersionText = QStringLiteral("5.1.2.2评估版本-4"); // RELEASE_META_VERSION_MARKER
+    const QString kReleaseVersionText = QStringLiteral("5.1.2.3评估版本"); // RELEASE_META_VERSION_MARKER
 
     // kReleaseBuildTimeText 作用：
     // - 欢迎页显示的精确编译时间；
